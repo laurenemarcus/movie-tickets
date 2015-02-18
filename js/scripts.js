325fee95425a707def39d2b5dabd977b72c4ecdb
Lauren Marcus and Ron Park <lauren.e.marcus@gmail.com>
2015-02-18 14:54:28 -0800

    Calculate ticket price based on movie selection.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+var Ticket = {
+  cost: 10,
+  price: function() {
+    if (this.movie === "Jurassic World 3D") {
+      this.cost += 5;
+    } else if (this.movie === "Half Baked") {
+      this.cost -= 2;
+    }
+
+    if (this.time === "Morning") {
+      this.cost -= 3;
+    } else if (this.time === "Afternoon") {
+      this.cost -= 2;
+    } else if (this.time === "Evening"){
+      this.cost += 3;
+    }
+
+    if (this.age === "Adult") {
+      this.cost += 2;
+    }
+
+    return this.cost;
+  }
+};
---
+++ spec/specs.js
@@ -0,0 +1,45 @@
+describe("Ticket", function() {
+  describe("cost", function () {
+    it("will return a cost of 10", function() {
+    var testTicket = Object.create(Ticket);
+    expect(testTicket.cost).to.equal(10);
+    });
+  });
+  describe("price", function() {
+    it("will return a higher cost if the movie selected is 3D", function() {
+      var testTicket = Object.create(Ticket);
+      testTicket.movie = "Jurassic World 3D";
+      expect(testTicket.price()).to.equal(15);
+    });
+    it("will return a lower cost if the movie selected is old", function() {
+      var testTicket = Object.create(Ticket);
+      testTicket.movie = "Half Baked";
+      expect(testTicket.price()).to.equal(8);
+    });
+    it("will return a lower cost if the movie time is in the morning", function() {
+      var testTicket = Object.create(Ticket);
+      testTicket.time = "Morning";
+      expect(testTicket.price()).to.equal(7);
+    });
+    it("will return a lower cost if the movie time is in the afternoon", function() {
+      var testTicket = Object.create(Ticket);
+      testTicket.time = "Afternoon";
+      expect(testTicket.price()).to.equal(8);
+    });
+    it("will return a higher cost if the movie time is in the night", function() {
+      var testTicket = Object.create(Ticket);
+      testTicket.time = "Evening";
+      expect(testTicket.price()).to.equal(13);
+    });
+    it("will return a higher cost if the movie goer is an adult", function() {
+      var testTicket = Object.create(Ticket);
+      testTicket.age = "Child";
+      expect(testTicket.price()).to.equal(7);
+    });
+    it("will return a higher cost if the movie goer is an adult", function() {
+      var testTicket = Object.create(Ticket);
+      testTicket.age = "Adult";
+      expect(testTicket.price()).to.equal(12);
+    });
+  });
+});
